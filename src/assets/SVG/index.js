bca5cf04ac92df12fb2f54cf65e40d80bb87327b
Puviyarasu <puviyarasu5341@gmail.com>
2024-11-14 12:22:57 +0530

    test

--- src/assets/SVG/index.js
+++ src/assets/SVG/index.js
@@ -1,7 +1,33 @@
 import WhiteBackgroundSVG from "./Background/WhiteLineBG.svg";
 import YellowBackgroundSVG from "./Background/YellowLineBG.svg";
 import BlueBackgroundSVG from "./Background/BlueLineBG.svg";
-import BlurContainerSVG from "./Others/BlurContainer.svg"
-import YellowDesignSVG from "./Others/YellowDesign.svg"
-import FireIconSVG from "./Icons/FireIcon.svg"
-export {WhiteBackgroundSVG,YellowBackgroundSVG,BlueBackgroundSVG,BlurContainerSVG,YellowDesignSVG,FireIconSVG};
+import BlurContainerSVG from "./Others/BlurContainer.svg";
+import YellowDesignSVG from "./Others/YellowDesign.svg";
+import FireIconSVG from "./Icons/FireIcon.svg";
+import G_Medal from "./Others/G_medal.svg";
+import S_Medal from "./Others/S_Medal.svg";
+import B_Medal from "./Others/B_Medal.svg";
+import Null_Medal from "./Others/Null_Medal.svg";
+import CUP from "./Others/CUP.svg";
+import VS from "./Others/VS.svg";
+import AI_Icon from "./Icons/AI_Icon.png";
+import feedback from "./Icons/feedback.png";
+import SVGLoader from "./Others/SVGLoader.jsx";
+
+export {
+  WhiteBackgroundSVG,
+  YellowBackgroundSVG,
+  BlueBackgroundSVG,
+  BlurContainerSVG,
+  YellowDesignSVG,
+  FireIconSVG,
+  B_Medal,
+  G_Medal,
+  S_Medal,
+  Null_Medal,
+  CUP,
+  AI_Icon,
+  feedback,
+  SVGLoader,
+  VS,
+};
